40bf2562680f1d0ea939d6e2dd5001b40729b934
MatthiasRoelandts <matthias_10@hotmail.com>
2016-10-09 18:03:39 +0200

    register updated

---
+++ app/src/main/java/com/example/matth/finalapp/RegisterActivity.java
@@ -0,0 +1,56 @@
+package com.example.matth.finalapp;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void register(View view){
+        new AsyncTask<Void, Void, User>() {
+            User user = new User();
+            @Override
+            protected void onPreExecute() {
+                EditText userFistname = (EditText) findViewById(R.id.editText);
+                EditText userLastname = (EditText) findViewById(R.id.editText2);
+                EditText userEmail = (EditText) findViewById(R.id.editText3);
+                EditText userPassword = (EditText) findViewById(R.id.editText4);
+                user.setName(userFistname.getText().toString());
+                user.setLastName(userLastname.getText().toString());
+                user.setEmail(userEmail.getText().toString());
+                user.setPassword(userPassword.getText().toString());
+            }
+            @Override
+            protected User doInBackground(Void... params) {
+                try {
+                    final String url = "http://10.0.2.2:8080/registerUser";
+
+                    RestTemplate restTemplate = new RestTemplate();
+                    // Add the Jackson and String message converters
+                    restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+                    restTemplate.getMessageConverters().add(new StringHttpMessageConverter());
+                    // Make the HTTP POST request, marshaling the request to JSON, and the response to a String
+                    User response = restTemplate.postForObject(url, user, User.class);
+                    return response;
+
+                } catch (Exception e) {
+                    Log.e("MainActivity", e.getMessage(), e);
+                }
+                return null;
+            }
+        }.execute();
+    }
+}
---
+++ app/src/main/java/com/example/matth/finalapp/User.java
@@ -0,0 +1,62 @@
+package com.example.matth.finalapp;
+
+/**
+ * Created by matth on 7/10/2016.
+ */
+
+public class User {
+    private String name;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String password;
+    private String token;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
