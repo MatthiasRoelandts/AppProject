5fe5842f0b45aae2c27f58a8a81a3d21aff3ba88
MatthiasRoelandts <matthias_10@hotmail.com>
2016-10-10 13:16:09 +0200

    menu added

--- app/src/main/java/com/example/matth/finalapp/MainActivity.java
+++ app/src/main/java/com/example/matth/finalapp/MainActivity.java
@@ -1,13 +1,72 @@
 package com.example.matth.finalapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    ActionBarDrawerToggle toggleLeft;
+    DrawerLayout  drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //set the main fragment
+        HomeMenuFragment homeMenuFragment = new HomeMenuFragment();
+        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, homeMenuFragment);
+        fragmentTransaction.commit();
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        toggleLeft = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggleLeft);
+        toggleLeft.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.menuLeft);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggleLeft.onOptionsItemSelected(item)) {
+            drawerLayout.closeDrawer(Gravity.RIGHT);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        android.support.v4.app.FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        switch (item.getItemId()) {
+            case R.id.nav_menu_1:
+                HomeMenuFragment homeMenuFragment = new HomeMenuFragment();
+                fragmentTransaction.replace(R.id.frameLayout, homeMenuFragment);
+                break;
+            case R.id.nav_menu_2:
+                Toast.makeText(MainActivity.this, "Delete item", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.settings:
+                Toast.makeText(MainActivity.this, "Delete item", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        fragmentTransaction.commit();
+        DrawerLayout dl = (DrawerLayout)  findViewById(R.id.drawerLayout);
+        if(dl.isDrawerOpen(GravityCompat.START)) {
+            dl.closeDrawer(GravityCompat.START);
+        }
+        return false;
     }
 }
