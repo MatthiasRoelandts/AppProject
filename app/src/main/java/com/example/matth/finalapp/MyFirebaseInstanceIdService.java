d18ff44693d62afaabf7a921d872f2c0a86eeb86
MatthiasRoelandts <matthias_10@hotmail.com>
2016-10-10 10:43:01 +0200

    notification support added (but not yet implemented)

---
+++ app/src/main/java/com/example/matth/finalapp/MyFirebaseInstanceIdService.java
@@ -0,0 +1,28 @@
+package com.example.matth.finalapp;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by matth on 10/10/2016.
+ */
+
+public class MyFirebaseInstanceIdService extends FirebaseInstanceIdService {
+
+    private static final String REG_TOKEN = "REG_TOKEN";
+    private String token;
+
+    @Override
+    public void onTokenRefresh() {
+
+        token = FirebaseInstanceId.getInstance().getToken();
+        Log.d(REG_TOKEN, token);
+        //registerToken(recent_token);
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
