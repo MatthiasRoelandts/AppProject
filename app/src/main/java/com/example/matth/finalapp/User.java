3f3e6062f18585bf9964c27290c30e82465a2499
MatthiasRoelandts <matthias_10@hotmail.com>
2016-10-13 14:29:19 +0200

    commit

---
+++ app/src/main/java/com/example/matth/finalapp/User.java
@@ -0,0 +1,71 @@
+package com.example.matth.finalapp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * Created by matth on 7/10/2016.
+ */
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class User {
+    private String name;
+    private String lastname;
+    private String email;
+    private String phone;
+    private String password;
+    private String token;
+
+    public User(String email,String password){
+
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
